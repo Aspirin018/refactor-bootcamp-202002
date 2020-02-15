dd846f6d6a12c5c11efe4ed24ce9bf3a5cf7bad6
Aspirin018 <438238118@qq.com>
2020-02-15 16:38:57 +0800

    删除多余代码和注释

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -16,22 +16,11 @@ public class OrderReceipt {
 
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
-
-        // print headers
         output.append("======Printing Orders======\n");
-
-        // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
-
         order.printItems(output);
-
-        // prints the state tax
         output.append("Sales Tax").append('\t').append(order.getTotalSalesTax());
-
-        // print total amount
         output.append("Total Amount").append('\t').append(order.getTotalAmount());
         return output.toString();
     }
